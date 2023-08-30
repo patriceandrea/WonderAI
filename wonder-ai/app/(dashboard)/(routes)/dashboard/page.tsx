4f6c25dccd45f6e114d87db1fef5727f90b11cdb
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-08-29 20:11:07 -0400

    setups folder routes

---
+++ wonder-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function DashboardPage() {
+  return (
+    <>
+    <h1>WonderAI</h1>
+    <h2>Welcome to the Dashboard Page || Protected </h2>
+      
+    </>
+  );
+}
+
---
+++ wonder-ai/app/(landing)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const LandingPage = (props: Props) => {
+  return (
+    <>
+    <h1>Wonder AI</h1>
+    <h2>Welcome to the Landing Page</h2>
+    <div>Landing Page || Unprotected </div>
+    </>
+  )
+}
+
+export default LandingPage
