a3ac96b3078fe99d90ec1af31291454f66afbe3d
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-09-05 22:56:03 -0400

    add auth when login or sign up

--- wonder-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ wonder-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function DashboardPage() {
   return (
     <>
     <h1>WonderAI</h1>
-    <h2>Welcome to the Dashboard Page || Protected </h2>
-      
+    <h2> Dashboard Page || Protected </h2>
+      <UserButton afterSignOutUrl="/"/>
     </>
   );
 }
