11762c0223a1c1c32676824ef7df5a1baa91b993
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-10-21 22:02:05 -0400

    adds prisma model and user api limit

---
+++ wonder-ai/lib/api-limit.ts
@@ -0,0 +1,46 @@
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNT } from "@/constants";
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (userApiLimit) {
+    await prismadb.userApiLimit.update({
+      where: { userId: userId },
+      data: { count: userApiLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: {
+        userId: userId,
+        count: 1,
+      },
+    });
+  }
+};
+
+
+export const checkApiLimit = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+      return false;
+    }
+
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userId : userId,
+        },
+        }); 
+}
