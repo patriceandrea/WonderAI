2ba901bee5570f00c55a2802e88d32f9edba18a5
Patrice Pavico <andrea_pavico98@yahoo.ca>
2023-11-12 17:07:35 -0500

    adds landing page

--- wonder-ai/app/(landing)/page.tsx
+++ wonder-ai/app/(landing)/page.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import LandingNavbar from "@/components/landing-navbar";
 import LandingHero from "@/components/landing-hero";
+import LandingContent from "@/components/landing-content";
 
 const LandingPage = () => {
   return (
     <div className="h-full">
       <LandingNavbar />
       <LandingHero />
+      <LandingContent/>
     </div>
   );
 };
